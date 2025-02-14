745ff38757a4e5da08a4f685a9ebff9902ead5a4
Magamed56 <94898458+Magamed56@users.noreply.github.com>
2025-02-14 13:03:27 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def echo(update: Update, context: CallbackContext) -> None:
 
 # Добавляем обработчики команд
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("Лекционная тема", tema))
+app.add_handler(CommandHandler("^Лекционная тема$", tema))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 # Запуск бота
